445102a17728406877cba5a537fd788cbd404fb5
Richard Stacey <staceyrichard1996@gmail.com>
2024-07-29 06:42:29 -0600

    Hard coded data array into app.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+let games = ["Game Boy - Nintendo Switch Online", "Game Boy Advance - Nintendo Switch Online", "Hyrule Warriors: Age of Calamity", "Lego City Undercover", "Lego Star Wars: The Skywalker Saga", "Mario Kart 8", "Nintendo 64 - Nintendo Switch Online", "Nintendo Entertainment System - Nintendo Switch Online", "Pokemon Violet", "Sega Genesis - Nintendo Switch Online", "Sega Genesis Classics", "Sonic x Shadow Generations", "Super Nintendo - Nintendo Switch Online", "Super Smash Bros Ultimate", "The Legend of Zelda: Breath of the Wild", "The Legend of Zelda: Echoes of Wisdom", "The Legend of Zelda: Link's Awakening", "The Legend of Zelda: Skyward Sword", "The Legend of Zelda: Tears of the Kingdom", "Transformers: Galactic Trials"];
+
 function App() {
 return (
   <>
